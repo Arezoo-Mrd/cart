9cc893538f75535fb3ac301de2b41c4ef9da6c2d
Arezoo Moradi <moradi.arezoo1992@gmail.com>
2021-06-20 11:12:25 +0430

    get cart data from localStorage

--- javascript/app.js
+++ javascript/app.js
@@ -6,11 +6,12 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartDOM = document.querySelector(".cart");
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
 let cart = [];
 class Product {
     async getProduct() {
         try {
-            const result = await fetch("../products.json");
+            const result = await fetch("products.json");
             const data = await result.json();
             let products = data.items;
             products = products.map((item) => {
@@ -67,10 +68,10 @@ class View {
         cart.map((item) => {
             totalPrice += item.price * item.amount
             totalItems += item.amount
-            cartTotal.innerText = totalPrice
-            cartItems.innerText = totalItems
-        })
 
+        })
+        cartTotal.innerText = totalPrice
+        cartItems.innerText = totalItems
     }
     addCartItem(item) {
         const div = document.createElement('div')
@@ -81,12 +82,12 @@ class View {
                 <div>
                     <h4>${item.title}</h4>
                     <h5>${item.price}</h5>
-                    <span class="remove-item">حذف</span>
+                    <span class="remove-item" data-id=${item.id}>حذف</span>
                 </div>
                 <div>
-                    <i class="fas-fa-chevron-up"></i>
+                    <i class="fas fa-chevron-up" data-id=${item.id}></i>
                     <p class="item-amount">${item.amount}</p>
-                    <i class="fas-fa-chevron-down"></i>
+                    <i class="fas fa-chevron-down" data-id=${item.id}></i>
                 </div>
         `
         cartContent.appendChild(div)
@@ -111,6 +112,71 @@ class View {
         cartOverlay.classList.remove('transparentBcg')
         cartDOM.classList.remove('showCart')
 
+    }
+    cartProcess() {
+        clearCart.addEventListener('click', () => {
+            this.clearCart()
+        })
+        cartContent.addEventListener('click', (event) => {
+            if (event.target.classList.contains('remove-item')) {
+                let removeItem = event.target
+                let id = removeItem.dataset.id
+                cartContent.removeChild(removeItem.parentElement.parentElement)
+                this.removeProduct(id)
+
+            }
+
+            if (event.target.classList.contains('fa-chevron-up')) {
+                let addtAmount = event.target
+                let id = addtAmount.dataset.id
+
+                let product = cart.find((item) => {
+                    return item.id === id
+                })
+                product.amount += 1
+                this.setCartValue(cart)
+                Storage.saveCart(cart)
+
+                addtAmount.nextElementSibling.innerText = product.amount
+            }
+            if (event.target.classList.contains('fa-chevron-down')) {
+                let downAmount = event.target
+                let id = downAmount.dataset.id
+
+                let product = cart.find((item) => {
+                    return item.id === id
+                })
+                product.amount += -1
+                if (product.amount > 0) {
+                    Storage.saveCart(cart)
+                    this.setCartValue(cart)
+                    downAmount.previousElementSibling.innerText = product.amount
+                } else {
+                    cartContent.removeChild(downAmount.parentElement.parentElement)
+                    this.removeProduct(id)
+                }
+
+
+            }
+        })
+    }
+    clearCart() {
+        let cartItems = cart.map((item) => {
+            return item.id
+        })
+        cartItems.forEach((item) => {
+            return this.removeProduct(item)
+        })
+        while (cartContent.children.length > 0) {
+            cartContent.removeChild(cartContent.children[0])
+        }
+    }
+    removeProduct(id) {
+        cart = cart.filter((item) => {
+            return item.id !== id
+        })
+        this.setCartValue(cart)
+        Storage.saveCart(cart)
     }
 }
 
@@ -142,5 +208,6 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         .then(() => {
             view.getCartButtons();
+            view.cartProcess()
         });
 });
